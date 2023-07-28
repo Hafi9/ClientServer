bbbaf451efb0318c9af4a3b7fa7c23544f207356
Hafi9 <71305958+Hafi9@users.noreply.github.com>
2023-07-28 17:18:20 +0700

    Pembaruan database (laptop habis bermasalah)

--- API/Migrations/20230728021303_CreateDatabase.Designer.cs
+++ API/Migrations/20230728021303_CreateDatabase.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(BookingDbContext))]
-    [Migration("20230718064046_newConvention")]
-    partial class newConvention
+    [Migration("20230728021303_CreateDatabase")]
+    partial class CreateDatabase
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -27,7 +27,6 @@ namespace API.Migrations
             modelBuilder.Entity("API.Models.Account", b =>
                 {
                     b.Property<Guid>("Guid")
-                        .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier")
                         .HasColumnName("guid");
 
@@ -86,6 +85,10 @@ namespace API.Migrations
 
                     b.HasKey("Guid");
 
+                    b.HasIndex("AccountGuid");
+
+                    b.HasIndex("RoleGuid");
+
                     b.ToTable("tb_m_account_roles");
                 });
 
@@ -129,13 +132,16 @@ namespace API.Migrations
 
                     b.HasKey("Guid");
 
+                    b.HasIndex("EmployeeGuid");
+
+                    b.HasIndex("RoomGuid");
+
                     b.ToTable("tb_tr_bookings");
                 });
 
             modelBuilder.Entity("API.Models.Education", b =>
                 {
                     b.Property<Guid>("Guid")
-                        .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier")
                         .HasColumnName("guid");
 
@@ -164,6 +170,8 @@ namespace API.Migrations
                         .HasColumnName("university_guid");
 
                     b.HasKey("Guid");
+
+                    b.HasIndex("UniversityGuid");
 
                     b.ToTable("tb_m_educations");
                 });
@@ -306,6 +314,103 @@ namespace API.Migrations
                     b.HasKey("Guid");
 
                     b.ToTable("tb_m_universities");
+                });
+
+            modelBuilder.Entity("API.Models.Account", b =>
+                {
+                    b.HasOne("API.Models.Employee", "Employee")
+                        .WithOne("Account")
+                        .HasForeignKey("API.Models.Account", "Guid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Employee");
+                });
+
+            modelBuilder.Entity("API.Models.AccountRole", b =>
+                {
+                    b.HasOne("API.Models.Account", "Account")
+                        .WithMany("AccountRoles")
+                        .HasForeignKey("AccountGuid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("API.Models.Role", "Role")
+                        .WithMany("AccountRoles")
+                        .HasForeignKey("RoleGuid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Account");
+
+                    b.Navigation("Role");
+                });
+
+            modelBuilder.Entity("API.Models.Booking", b =>
+                {
+                    b.HasOne("API.Models.Employee", "Employee")
+                        .WithMany("Bookings")
+                        .HasForeignKey("EmployeeGuid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("API.Models.Room", "Room")
+                        .WithMany("Bookings")
+                        .HasForeignKey("RoomGuid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Employee");
+
+                    b.Navigation("Room");
+                });
+
+            modelBuilder.Entity("API.Models.Education", b =>
+                {
+                    b.HasOne("API.Models.Employee", "Employee")
+                        .WithOne("Education")
+                        .HasForeignKey("API.Models.Education", "Guid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("API.Models.University", "University")
+                        .WithMany("Educations")
+                        .HasForeignKey("UniversityGuid")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Employee");
+
+                    b.Navigation("University");
+                });
+
+            modelBuilder.Entity("API.Models.Account", b =>
+                {
+                    b.Navigation("AccountRoles");
+                });
+
+            modelBuilder.Entity("API.Models.Employee", b =>
+                {
+                    b.Navigation("Account");
+
+                    b.Navigation("Bookings");
+
+                    b.Navigation("Education");
+                });
+
+            modelBuilder.Entity("API.Models.Role", b =>
+                {
+                    b.Navigation("AccountRoles");
+                });
+
+            modelBuilder.Entity("API.Models.Room", b =>
+                {
+                    b.Navigation("Bookings");
+                });
+
+            modelBuilder.Entity("API.Models.University", b =>
+                {
+                    b.Navigation("Educations");
                 });
 #pragma warning restore 612, 618
         }
