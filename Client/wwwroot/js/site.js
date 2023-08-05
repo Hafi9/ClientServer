0bf7cd9da3858114b121282e1b0ed4b7b0d00b35
Hafi9 <71305958+Hafi9@users.noreply.github.com>
2023-08-06 05:00:09 +0700

    Pokemon API

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -73,50 +73,69 @@ console.log(extractOnlyCat(animals));//cat saja
 console.log(extractOnlyFish(animals));//fish saja*/
 
 //asynchronous javascript
-$.ajax({
-    url: "https://swapi.dev/api/people/"
-}).done((result) => {
-    let temp = "";
-    $.each(result.results, (key, val) => {
-        temp += "<li>" + val.name + "</li>";
-    })
-    $("#listSW").html(temp);
+const pokedex = document.getElementById('pokedex');
+const modal = document.getElementById('modal');
+const modalName = document.getElementById('modal-name');
+const modalImage = document.getElementById('modal-image');
+const modalNumber = document.getElementById('modal-number');
+const modalTypes = document.getElementById('modal-types');
+const modalAbilities = document.getElementById('modal-abilities');
+const modalHP = document.getElementById('modal-hp');
+const modalSpeed = document.getElementById('modal-speed');
+const closeBtn = document.getElementById('close');
+
+const fetchPokemons = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=12');
+        const data = await response.json();
+
+        const pokemons = data.results;
+        pokemons.forEach(pokemon => {
+          fetchPokemonData(pokemon);
+        });
+      } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+      }
+    };
+
+    const fetchPokemonData = async (pokemon) => {
+      try {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+
+        const row = document.createElement('tr');
+        row.classList.add('pokemon');
+        row.innerHTML = `
+          <td>${data.name}</td>
+          <td><img src="${data.sprites.front_default}" alt="${data.name}"></td>
+          <td>#${data.id}</td>
+        `;
+
+        row.addEventListener('click', () => {
+          modalName.textContent = data.name;
+          modalImage.src = data.sprites.front_default;
+          modalNumber.textContent = `#${data.id}`;
+          modalTypes.textContent = `Types: ${data.types.map(type => type.type.name).join(', ')}`;
+          modalAbilities.textContent = `Abilities: ${data.abilities.map(ability => ability.ability.name).join(', ')}`;
+          modalHP.textContent = `HP: ${data.stats[0].base_stat}`;
+          modalSpeed.textContent = `Speed: ${data.stats[5].base_stat}`;
+          modal.style.display = 'block';
+        });
+
+        pokedex.appendChild(row);
+      } catch (error) {
+        console.error('Error fetching Pokemon details:', error);
+      }
+    };
+
+    closeBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 });
 
-$(document).ready(function () {
-    $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon/"
-    }).done((result) => {
-        let temp = "";
-        $.each(result.results, (key, val) => {
-            temp += `
-                <tr>
-                    <td>${key + 1}</td>
-                    <td>${val.name}</td>
-                    <td>${val.url}</td>
-                    <td>
-                        <button onclick="detailPokemon('${val.url}')" data-bs-toggle="modal" data-bs-target="#modalPokemon" class="btn btn-primary">Detail</button>
-                    </td>
-                </tr>
-            `;
-        })
-        $("#tbodyPokemon").html(temp);
-    });
-});
-
-function detailPokemon(stringURL) {
-    $.ajax({
-        url: stringURL,
-        success: (result) => {
-            $('.modal-title').html(result.name);
-            $('#pokemonImage').attr('src', result.sprites.front_default);
-            $('#pokemonImage').css('max-width', '100%');
-            $('.details').html(`
-                <p><strong>Height:</strong> ${result.height}</p>
-                <p><strong>Weight:</strong> ${result.weight}</p>
-                <p><strong>Base Experience:</strong> ${result.base_experience}</p>
-                <!-- Add more details as needed -->
-            `);
-        }
-    });
-}
+fetchPokemons();
